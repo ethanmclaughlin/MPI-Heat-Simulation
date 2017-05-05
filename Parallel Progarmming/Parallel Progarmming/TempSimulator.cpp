6935f7050741f5838b327b27b9b8d059c141da67
ethanmclaughlin <ethan.mclaughlin@students.fhu.edu>
2017-05-05 09:17:38 -0500

    Fixed input file reader and 2d vectors. Started Calculation function

--- Parallel Progarmming/Parallel Progarmming/TempSimulator.cpp
+++ Parallel Progarmming/Parallel Progarmming/TempSimulator.cpp
@@ -18,98 +18,55 @@ struct point
 };
 
 
-vector<vector<point>> initial(vector<float> temperatures, int X_dim, int Y_dim) {
+vector<vector<point>> initial(vector<vector<float>> temperatures, int X_dim, int Y_dim) {
 
 	vector<vector<point>> initial_grid;
 
-	vector<vector<float>> temps_2D;
-
-	temps_2D = vector<vector<float>>(X_dim, vector<float>(Y_dim));
-
 	initial_grid = vector<vector<point>>(X_dim, vector<point>(Y_dim));
 
-	for (int k = 0; k < temperatures.size(); k++) {
-		temps_2D[k / X_dim][k % X_dim] = temperatures[k];
-		string x = to_string(temps_2D[k / X_dim][k % X_dim]);
-		cout << x << endl;
-	}
 
-	for (int i = 0; i < X_dim; i++) {
-		for (int j = 0; j < Y_dim; j++) {
+	for (int r = 0; r < Y_dim; r++) {
+		for (int c = 0; c < X_dim; c++) {
 			point temp;
-			if (i == 0 && j == 0) {
+			if (r == 0 && c == 0) {
 				temp.IS_static = true;
 			}
 			else {
 				temp.IS_static = false;
 			}
-			temp.Temp = temps_2D[j][i];		
-			initial_grid[j][i] = temp;
+			temp.Temp = temperatures[r][c];		
+			initial_grid[r][c] = temp;
 		}
 	}
 	return initial_grid;
 }
 
-/*
-void print(double room[SIZE_OF_ARRAY][SIZE_OF_ARRAY]) {
-	for (int row = 0; row < SIZE_OF_ARRAY; row++)
-	{
-		for (int colomn = 0; colomn < SIZE_OF_ARRAY; colomn++)
-		{
-			if (colomn == 9) {
-				cout << room[row][colomn];
-			}
-			else {
-				cout << room[row][colomn] << ",";
-			}
-		}
-		cout << endl;
-	}
-}*/
-/*
-void firstIteration(double room[SIZE_OF_ARRAY][SIZE_OF_ARRAY])
-{
-	for (int row = 1; row < SIZE_OF_ARRAY - 1; row++)
-	{
-		for (int column = 1; column < SIZE_OF_ARRAY - 1; column++)
-		{
 
-			averageTemperature = (room[row][column - 1] + room[row + 1][column] + room[row][column + 1] + room[row - 1][column]) / NEIGHBORS;
-			room[row][column] = averageTemperature;
+void temp_simulator(vector<vector<point>> grid, int X_dim, int Y_dim, int iteration, float multiplier) {
 
-		}
-	}
-
-}
-
-void average(double room[SIZE_OF_ARRAY][SIZE_OF_ARRAY])
-{
-	do
-	{
-		largestChange = 0;
-		for (int row = 1; row < SIZE_OF_ARRAY - 1; row++)
-		{
-			for (int column = 1; column < SIZE_OF_ARRAY - 1; column++)
-			{
-				double oldValue = room[row][column];
-				averageTemperature = (room[row][column - 1] + room[row + 1][column] + room[row][column + 1] + room[row - 1][column]) / NEIGHBORS;
-				room[row][column] = averageTemperature;
-
-				double change = abs(oldValue - room[row][column]);
-				if (change > largestChange)
-				{
-					largestChange = change;
+	for (int i = 0; i > iteration; i++) {
+		for (int r = 0; r < Y_dim; r++) {
+			for (int c = 0; c < X_dim; c++) {
+				if (!grid[c][r].IS_static) {
+					if ((c == 0 && r == 0) || (c == X_dim - 1 && r == Y_dim - 1) || (c == 0 && r == Y_dim - 1) || (c == Y_dim - 1 && r == 0)) {
+						//corner formula
+					}
+					else if ((c == 0 && r < Y_dim) || (c == X_dim - 1 && r < Y_dim) || (c < X_dim - 1 && r == 0) || (c < X_dim - 1 && r == Y_dim - 1) {
+						//edge formula
+					}
+					else {
+						//middle formula
+					}
 				}
+
 			}
 		}
-
-
-	} while (largestChange > CHANGE);
+	}
 
 }
-*/
 
-vector<string> file_reader() {	
+
+vector<vector<float>> file_reader(int&X_dim, int&Y_dim, int&iterations, float&multiplier) {
 	string line;
 	int counter = 0;
 
@@ -118,14 +75,42 @@ vector<string> file_reader() {
 
 	if (inputfile.is_open())
 	{		
-		vector<string> input_data(counter);
 		
-		while (getline(inputfile, line)) {
-			input_data.push_back(line);
-			cout << "added " << line << " to vector \n";
+		string x_string, y_string, iter_string, multi_string;
+
+		getline(inputfile, line);
+		x_string = line;
+		cout << x_string << endl;
+		X_dim = stoi(x_string);
+
+		getline(inputfile, line);
+		y_string = line;
+		cout << y_string << endl;
+		Y_dim = stoi(y_string);
+
+		getline(inputfile, line);
+		iter_string = line;
+		cout << iter_string << endl;
+		iterations = stoi(iter_string);
+
+		getline(inputfile, line);
+		multi_string = line;
+		cout << multi_string << endl;
+		multiplier = stof(multi_string);
+
+		vector<vector<float>> input_data = vector<vector<float>>(X_dim, vector<float>(Y_dim));
+
+		for (int r = 0; r < X_dim; r++) {
+			for (int c = 0; c < Y_dim; c++) {
+				getline(inputfile, line);
+				cout << line << endl;
+				input_data[c][r] = stof(line);
+				cout << "Added as float" << endl;
+			}
 		}
+
 		inputfile.close();
-		cout << "added all data to vector!\n";
+		cout << "added all data to vector!" << endl;
 		return input_data;
 		
 		
@@ -137,236 +122,27 @@ int main() {
 
 	float NewTemp;
 
-	int X_dim, Y_dim, iterations;
-	float multiplier;
+	int X_dim = 0;
+	int	Y_dim = 0;
+	int	iterations = 0;
+	float multiplier = 0;
 	float temp;
 	//Use dynamic array
 
-	vector<string> input_data = file_reader();
-
-	X_dim = stoi(input_data[0]);
-	cout << input_data[0] << endl;
-	Y_dim = stoi(input_data[1]);
-	cout << input_data[1] << endl;
-	iterations = stoi(input_data[2]);
-	cout << input_data[2] << endl;
-	multiplier = stof(input_data[3]);
-	cout << input_data[3] << endl << endl;
-
-	vector<float> temperatures;
-	
-
-	for (int i = 4; i < input_data.size(); i++) {
-			cout << input_data[i] << endl;
-			temp = stof(input_data[i]);
-				
-	}
+	vector<vector<float>> input_data = file_reader(X_dim, Y_dim, iterations, multiplier);
 
 	cout << endl;
 
-	vector<vector<point>> initial_grid = initial(temperatures, X_dim, Y_dim);
+	vector<vector<point>> initial_grid = initial(input_data, X_dim, Y_dim);
 
 	for (int i = 0; i < X_dim; i++) {
 		for (int j = 0; j < Y_dim; j++) {
-			cout << initial_grid[j][i].Temp << endl;
+			cout << to_string(initial_grid[j][i].Temp) << endl;
 		}
 	}
+
+
 
 	system("PAUSE");
 
 }
-	/*point room[SIZE_OF_ARRAY][SIZE_OF_ARRAY];
-	cout << "Temp simulator" << endl;
-	cout << endl;
-	cout << "Printing initial temp..." << endl;
-	//initial(room);
-	//print(room);
-
-
-	int room[10][10];
-	//float formula = P1Temp + ((P2Temp - P1Temp) * .01);
-
-	//populate initial grid
-	for (int row = 0; row < room[10][10]; row++)
-	{
-		for (int column = 0; column < room[10][10]; column++)
-		{
-			if (row == 0 || row == 9)
-			{
-				if (column == 0 || column == 9)
-				{
-					room[row][column] = 0;
-				}
-				else
-				{
-					room[row][column] = 0;
-				}
-			}
-			else
-			{
-				room[row][column] = 0;
-			}
-		}
-	}*/
-
-	//print initial grid
-	/*for (int row = 0; row < 10; row++)
-	{
-		for (int column = 0; column < 10; column++)
-		{
-			cout << room[row][column] << " ";
-		}
-		cout << endl;
-	}
-	
-	int temp;
-
-	cout << "Enter temperture: ";
-	cin >> temp;
-
-	//First iteration
-	for (int row = 0; row < room[10][10]; row++)
-	{
-		for (int column = 0; column < room[10][10]; column++)
-		{
-			//top or bottom row
-			if (row == 0 || row == 9)
-			{
-				//first or last column
-				if (column == 0 || column == 9)
-				{
-					//Corner formula
-					//P1Temp +((P2Temp - P1Temp) * .01) + ((P3Temp - P1Temp) * .01)
-					//NewTemp = room[row][column] + ((room[row][column + 1] - room[row][column]) * .01) + ((room[row + 1][column] - room[row][column]) * .01);
-					
-					//Top Right Corner
-					NewTemp = room[row][column] + ((room[row][column - 1] - room[row][column]) * .01) + ((room[row + 1][column] - room[row][column]) * .01);
-					//Bottom Left Corner
-					NewTemp = room[row][column] + ((room[row][column + 1] - room[row][column]) * .01) + ((room[row - 1][column] - room[row][column]) * .01);
-					//Bottom Right Corner
-					NewTemp = room[row][column] + ((room[row][column - 1] - room[row][column]) * .01) + ((room[row - 1][column] - room[row][column]) * .01);
-				}
-				else
-				{
-					//Edge formula
-					//P1Temp +((P2Temp - P1Temp) * .01) + ((P3Temp - P1Temp) * .01) + ((P4Temp - P1Temp) * .01)
-
-					//Top Edge
-					NewTemp = room[row][column] + ((room[row][column - 1] - room[row][column]) * .01) + ((room[row][column + 1] - room[row][column]) * .01)
-						+ ((room[row + 1][column] - room[row][column]) * .01);
-					//Left Edge
-					NewTemp = room[row][column] + ((room[row - 1][column] - room[row][column]) * .01) + ((room[row][column + 1] - room[row][column]) * .01)
-						+ ((room[row + 1][column] - room[row][column]) * .01);
-					//Right Edge
-					NewTemp = room[row][column] + ((room[row - 1][column] - room[row][column]) * .01) + ((room[row][column - 1] - room[row][column]) * .01)
-						+ ((room[row + 1][column] - room[row][column]) * .01);
-					//Bottom Edge
-					NewTemp = room[row][column] + ((room[row][column - 1] - room[row][column]) * .01) + ((room[row - 1][column] - room[row][column]) * .01)
-						+ ((room[row][column + 1] - room[row][column]) * .01);
-				}
-			}
-			else
-			{
-				//Middle formula
-				//P1Temp +((P2Temp - P1Temp) * .01) + ((P3Temp - P1Temp) * .01) + ((P4Temp - P1Temp) * .01) + ((P5Temp - P1Temp) * .01)
-				NewTemp = room[row][column] + ((room[row][column - 1] - room[row][column]) * .01) + ((room[row][column + 1] - room[row][column]) * .01)
-					+ ((room[row + 1][column] - room[row][column]) * .01) + ((room[row - 1][column] - room[row][column]) * .01);
-			}
-		}
-	}
-
-	//Print final grid
-	for (int row = 0; row < 10; row++)
-	{
-		for (int column = 0; column < 10; column++)
-		{
-			cout << room[row][column] << " ";
-		}
-		cout << endl;
-	}
-
-
-	system("PAUSE");
-	return 0;
-}
-
-
-/*
-float fun(int a)
-{
-	float formula = 4 * a - (a*a) / 2.0;
-
-	return formula;
-}
-
-int main()
-{
-	float u[X + 1][T + 1];
-	float h = 1.0;
-	float k = 0.0125;
-	float c;
-	float al;
-	float us;
-	float ue;
-
-
-	cout << "\n Enter the square of 'c' :";
-	cin >> c;
-
-	al = c*k / pow(h, 2);
-
-	cout << " Enter the value of u[0, t] :";
-	cin >> us;
-	cout << "  Enter the value of u[" << X << ",t]:";
-	cin >> ue;
-
-	for (int i = 0; i <= T; i++)
-	{
-		u[0][i] = u[X][i] = us;
-	}
-
-	for (int j = 1; j <= X - 1; j++)
-	{
-		u[j][0] = fun(j);
-	}
-
-	for (int i = 0; i <= T - 1; i++)
-	{
-		for (int j = 1; j <= X - 1; j++)
-		{
-			u[j][i + 1] = al*u[j - 1][i] + (1 - 2 * al)*u[j][i] + al*u[j + 1][i];
-		}
-	}
-
-	cout << " The value of alpha is " << al << "\n";
-	cout << " The value of u[j,i] are:\n\n ";
-	/*
-	for (int i = 0; i < T; i++)
-	{
-		for (int j = 0; j < X; j++)
-			cout << "7.4 \t" << u[j][i];
-		cout << "\n";
-	}
-	*//*
-	for (int row = 0; row < T; row++)
-	{
-		for (int column = 0; column < X; column++)
-		{
-			//cout <<  setprecision(3) << u[row][column];
-			printf("%6.4f\t", u[column][row]);
-		}
-
-		cout << endl;
-	}
-
-	system("PAUSE");
-	return 0;
-}*/
-
-
-/*
-P1NewTemp = P1Temp + ((P2Temp - P1Temp) * .01)
-
-
-
-*/
